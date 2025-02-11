1b7681de7d31563e24dfc76b28bf4d643426004b
Rick Clare <rickclare@users.noreply.github.com>
2025-02-11 10:18:21 +0000

    add Counter live

--- lib/lunch_web/live/counter_live.ex
+++ lib/lunch_web/live/counter_live.ex
@@ -1,4 +1,16 @@
 defmodule LunchWeb.CounterLive do
   @moduledoc false
   use LunchWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :count, 0)}
+  end
+
+  def handle_event("increment", _value, socket) do
+    {:noreply, update(socket, :count, fn i -> i + 1 end)}
+  end
+
+  def handle_event("decrement", _value, socket) do
+    {:noreply, update(socket, :count, fn i -> i - 1 end)}
+  end
 end
