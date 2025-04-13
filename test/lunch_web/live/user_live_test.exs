72c8aea9501874bd366af7815b308d9baa138277
Rick Clare <rickclare@users.noreply.github.com>
2025-04-13 17:42:51 +0100

    add User Authentication

--- test/lunch_web/live/user_live_test.exs
+++ test/lunch_web/live/user_live_test.exs
@@ -1,12 +1,13 @@
 defmodule LunchWeb.UserLiveTest do
   use LunchWeb.ConnCase
 
-  import Phoenix.LiveViewTest
   import Lunch.AccountsFixtures
+  import Phoenix.LiveViewTest
 
-  @create_attrs %{name: "some name"}
-  @update_attrs %{name: "some updated name"}
+  @create_attrs %{email: unique_user_email(), name: "some name"}
+  @update_attrs %{email: unique_user_email(), name: "some updated name"}
   @invalid_attrs %{name: nil}
+
   defp create_user(_) do
     user = user_fixture()
 
@@ -17,20 +18,20 @@ defmodule LunchWeb.UserLiveTest do
     setup [:create_user]
 
     test "lists all users", %{conn: conn, user: user} do
-      {:ok, _index_live, html} = live(conn, ~p"/users")
+      {:ok, _index_live, html} = live(conn, ~p"/ops/users")
 
       assert html =~ "Listing Users"
       assert html =~ user.name
     end
 
     test "saves new user", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"/users")
+      {:ok, index_live, _html} = live(conn, ~p"/ops/users")
 
       assert {:ok, form_live, _} =
                index_live
                |> element("a", "New User")
                |> render_click()
-               |> follow_redirect(conn, ~p"/users/new")
+               |> follow_redirect(conn, ~p"/ops/users/new")
 
       assert render(form_live) =~ "New User"
 
@@ -42,7 +43,7 @@ defmodule LunchWeb.UserLiveTest do
                form_live
                |> form("#user-form", user: @create_attrs)
                |> render_submit()
-               |> follow_redirect(conn, ~p"/users")
+               |> follow_redirect(conn, ~p"/ops/users")
 
       html = render(index_live)
       assert html =~ "User created successfully"
@@ -50,13 +51,13 @@ defmodule LunchWeb.UserLiveTest do
     end
 
     test "updates user in listing", %{conn: conn, user: user} do
-      {:ok, index_live, _html} = live(conn, ~p"/users")
+      {:ok, index_live, _html} = live(conn, ~p"/ops/users")
 
       assert {:ok, form_live, _html} =
                index_live
                |> element("#users-#{user.id} a", "Edit")
                |> render_click()
-               |> follow_redirect(conn, ~p"/users/#{user}/edit")
+               |> follow_redirect(conn, ~p"/ops/users/#{user}/edit")
 
       assert render(form_live) =~ "Edit User"
 
@@ -68,7 +69,7 @@ defmodule LunchWeb.UserLiveTest do
                form_live
                |> form("#user-form", user: @update_attrs)
                |> render_submit()
-               |> follow_redirect(conn, ~p"/users")
+               |> follow_redirect(conn, ~p"/ops/users")
 
       html = render(index_live)
       assert html =~ "User updated successfully"
@@ -76,7 +77,7 @@ defmodule LunchWeb.UserLiveTest do
     end
 
     test "deletes user in listing", %{conn: conn, user: user} do
-      {:ok, index_live, _html} = live(conn, ~p"/users")
+      {:ok, index_live, _html} = live(conn, ~p"/ops/users")
 
       assert index_live |> element("#users-#{user.id} a", "Delete") |> render_click()
       refute has_element?(index_live, "#users-#{user.id}")
@@ -87,20 +88,20 @@ defmodule LunchWeb.UserLiveTest do
     setup [:create_user]
 
     test "displays user", %{conn: conn, user: user} do
-      {:ok, _show_live, html} = live(conn, ~p"/users/#{user}")
+      {:ok, _show_live, html} = live(conn, ~p"/ops/users/#{user}")
 
       assert html =~ "Show User"
       assert html =~ user.name
     end
 
     test "updates user and returns to show", %{conn: conn, user: user} do
-      {:ok, show_live, _html} = live(conn, ~p"/users/#{user}")
+      {:ok, show_live, _html} = live(conn, ~p"/ops/users/#{user}")
 
       assert {:ok, form_live, _} =
                show_live
                |> element("a", "Edit")
                |> render_click()
-               |> follow_redirect(conn, ~p"/users/#{user}/edit?return_to=show")
+               |> follow_redirect(conn, ~p"/ops/users/#{user}/edit?return_to=show")
 
       assert render(form_live) =~ "Edit User"
 
@@ -112,7 +113,7 @@ defmodule LunchWeb.UserLiveTest do
                form_live
                |> form("#user-form", user: @update_attrs)
                |> render_submit()
-               |> follow_redirect(conn, ~p"/users/#{user}")
+               |> follow_redirect(conn, ~p"/ops/users/#{user}")
 
       html = render(show_live)
       assert html =~ "User updated successfully"
