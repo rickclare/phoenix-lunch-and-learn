8e7bfa14518afdae96fb92c5145b1794687a0c41
Rick Clare <rickclare@users.noreply.github.com>
2025-04-13 17:42:51 +0100

    add User Authentication

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Lunch.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.8.0-rc", override: true},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
